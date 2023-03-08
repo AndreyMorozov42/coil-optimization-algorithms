ec19090f889be82009f94f7681d0465bafac8a7f
Andrey Morozov <and.morozov.v@gmail.com>
2023-03-08 12:20:25 +0300

    update class hill climbing and jupyter notebook

--- optimization/hill_climbing_algorithms/steepest_ascent_hill_climbing.py
+++ optimization/hill_climbing_algorithms/steepest_ascent_hill_climbing.py
@@ -7,8 +7,10 @@ class SteepestAscentHillClimbing:
         self.start = start
         self.finish = finish
         self.fitness = fitness
-
         self.x0 = np.random.uniform(low=self.start, high=self.finish)
+        self.good_mutation = []
+        self.bad_mutation = []
+        self.all_mutation = []
 
     def mutation(self, x, r=1):
         return np.random.uniform(low=self.start if x - r < self.start else x - r,
@@ -16,13 +18,13 @@ class SteepestAscentHillClimbing:
 
     def run(self):
         for i in range(self.iteration):
-            fit = self.fitness(self.x0)
+            fit = self.fitness(R1=self.x0)
             xq = self.mutation(self.x0)
-            fit_xq = self.fitness(xq)
-
-            x = xq if fit_xq > fit else self.x0
-
-            if self.x0 == x:
-                self.x0 = self.mutation(self.x0)
+            fit_xq = self.fitness(R1=xq)
+            if fit_xq >= fit:
+                self.good_mutation.append((xq, fit_xq))
+                self.x0 = xq
             else:
-                self.x0 = x
+                self.bad_mutation.append((xq, fit_xq))
+            self.all_mutation.append((xq, fit_xq))
+        return self.x0
