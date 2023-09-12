eb650b5abe40ba508e797cbf491d4861bf4b651b
Andrey Morozov <and.morozov.v@gmail.com>
2023-09-12 11:37:05 +0300

    update test algorithm

--- hill_climbing_optimization/one_coil/ahc_one_coil.py
+++ hill_climbing_optimization/one_coil/ahc_one_coil.py
@@ -1,48 +1,58 @@
 import numpy as np
 
-from functions import mutual_inductance, self_inductance, coupling_coefficient
+from functions import coupling_coefficient
 from tools.mutation import mutation_lb
 
+
 def adaptive_hill_climbing(coil_t, coil_r, r_turn, d):
+    # mutation counter
     all_mutation = 0
     good_mutation = 0
     bad_mutation = 0
 
-    thr = 0.5e-3
+    # objective function increment threshold
+    thr = 1e-3
 
     fit_k = coupling_coefficient(coil_1=coil_t, r1_turn=r_turn,
                                  coil_2=coil_r, r2_turn=r_turn,
                                  dist=d)
 
-    # random generate two internal turns in coil
-    coil_tq = coil_t.copy()
-    fit_kq = 0
+    print(f"Initial Coupling coefficient {fit_k} for coils:\n"
+          f"coil1 = {coil_t}\n"
+          f"coil2 = {coil_r}\n")
 
-    i = 0
+    # initialization of initial values
+    fit_kq = 0
+    coil_tq = coil_t.copy()
+
+    # probability of mutation
     rm = np.random.random()
 
-    print(rm)
+    i = 0  # iteration counter
+    limit = 1000
 
-    while np.abs(fit_kq - fit_k) > thr:
+    while (np.array_equal(coil_t, coil_tq) or np.abs(fit_kq - fit_k) > thr) and i != limit:
 
         i += 1
 
-        if fit_kq > fit_k:
+        if fit_kq >= fit_k:
+            print(f"{i}: Found a new maximum value of the coupling coefficient: {fit_kq}")
             coil_t = coil_tq.copy()
             fit_k = fit_kq.copy()
             good_mutation += 1
-            print(f"{i} find good mutation")
         elif fit_kq != 0:
             bad_mutation += 1
-            # print(f"{i} find bad mutation")
+
+        coil_tq = coil_t.copy()
 
         for ind in range(1, len(coil_tq) - 1):
+            # probability of mutation for a turn
             r = np.random.random()
             if r < rm:
+                # mutate the coil
                 coil_tq[ind] = mutation_lb(start=coil_t[ind - 1] + 2 * r_turn,
-                                       finish=coil_t[ind + 1] - 2 * r_turn,
-                                       x=coil_tq[ind].copy(),
-                                       dr_max=0.05)
+                                           finish=coil_t[ind + 1] - 2 * r_turn,
+                                           x=coil_tq[ind].copy())
 
         fit_kq = coupling_coefficient(coil_1=coil_tq, r1_turn=r_turn,
                                       coil_2=coil_r, r2_turn=r_turn,
@@ -50,76 +60,108 @@ def adaptive_hill_climbing(coil_t, coil_r, r_turn, d):
 
         all_mutation += 1
 
-        if i > 500:
-            break
-
     if fit_kq > fit_k:
+        print(f"{i}: Found a new maximum value of the coupling coefficient: {fit_kq}")
         coil_t = coil_tq.copy()
         fit_k = fit_kq.copy()
         good_mutation += 1
-        print(f"{i} find good mutation")
     else:
         bad_mutation += 1
 
-    print(f"All iterations: {i}")
+    print(f"Stop at {i} iterations\n")
+
     return coil_t.copy(), fit_k.copy(), all_mutation, bad_mutation, good_mutation
 
 
 def launch(iterations, coil_t, coil_r, r_turn, d):
-    bad_mutation = []
-    good_mutation = []
-    all_mutation = []
-    fit_k = []
-
-    allm = 0
-    badm = 0
-    goodm = 0
-    k = 0
+    # array of mutation counters
+    arr_good = np.array([])
+    arr_bad = np.array([])
+    arr_all = np.array([])
+    fit = []
 
     for _ in range(iterations):
 
-        coil_t, k, allm, badm, goodm = adaptive_hill_climbing(
+        coil, k, allm, badm, goodm = adaptive_hill_climbing(
             coil_t=coil_t,
             coil_r=coil_r,
             r_turn=r_turn,
             d=d
         )
 
-        bad_mutation.append(badm)
-        good_mutation.append(goodm)
-        all_mutation.append(allm)
-        fit_k.append(k)
+        arr_all = np.append(arr_all, allm)
+        arr_good = np.append(arr_good, goodm)
+        arr_bad = np.append(arr_bad, badm)
 
-    bad_mutation = np.mean(np.array(bad_mutation))
-    good_mutation = np.mean(np.array(good_mutation))
-    all_mutation = np.mean(np.array(all_mutation))
+        fit.append((coil, k))
 
-    return max(fit_k), all_mutation, bad_mutation, good_mutation
+    # calculate characteristics of series
+    mean_agb = (np.average(arr_all), np.average(arr_good), np.average(arr_bad))
+    median_agb = (np.median(arr_all), np.median(arr_good), np.mean(arr_bad))
+    deviation_agb = (np.std(arr_all), np.std(arr_good), np.std(arr_bad))
+
+    # min and max values of the coupling coefficient
+    # and their corresponding coils
+    fit_values = (min(fit, key=lambda x: x[1]), max(fit, key=lambda x: x[1]))
+
+    return fit_values, mean_agb, median_agb, deviation_agb
 
 
 def main():
-    coil_t = np.linspace(start=0.02, stop=0.05, num=4)
-    coil_r = np.linspace(start=0.03, stop=0.09, num=4)
-    r_turn = 0.0004
+    coil_t = np.linspace(start=0.02, stop=0.05, num=4)  # transmitting coil
+    coil_r = np.linspace(start=0.03, stop=0.09, num=4)  # receiving coil
+    r_turn = 0.0004  # radius of coil turns
+
+    # distance
     d = 0.01
 
+    '''
+    ------------------------------------------------------------
+    Testing the algorithm for Hill Climbing in one run.
+    ------------------------------------------------------------
+    '''
     # coil_t, k, allm, badm, goodm = adaptive_hill_climbing(
     #     coil_t=coil_t,
     #     coil_r=coil_r,
     #     r_turn=r_turn,
     #     d=d
     # )
-    # print(f"coil_t={coil_t} k={k}")
-    # print(f"All mutation {allm}")
-    # print(f"Good mutation {goodm}")
-    # print(f"Bad mutation {badm}")
+    # print(f"The resulting value of the coupling coefficient: k={k}\n"
+    #       f"for coil_t={coil_t} м and coil_r={coil_r} м\n")
+    # print(f"All mutation: {allm}")
+    # print(f"Good mutation: {goodm}")
+    # print(f"Bad mutation: {badm}\n")
+    '''
+    ------------------------------------------------------------
+    Testing the algorithm for climbing
+    to the top of a hill on several runs.
+    ------------------------------------------------------------
+    '''
+    iterations = 100
+    fit_values, mean_agb, median_agb, deviation_agb = launch(
+        iterations=iterations,
+        coil_t=coil_t,
+        coil_r=coil_r,
+        r_turn=r_turn,
+        d=d)
 
-    k, a, g, b = launch(iterations=1000, coil_t=coil_t, coil_r=coil_r, r_turn=r_turn, d=d)
+    print(f"Average good mutation: {mean_agb[1]}")
+    print(f"Average bad mutation: {mean_agb[2]}")
+    print(f"Average all mutation: {mean_agb[0]}\n")
 
-    print(f"All mutation {a}")
-    print(f"Good mutation {g}")
-    print(f"Bad mutation {b}")
-    print(f"Max k = {k}")
+    print(f"Median good mutation: {median_agb[1]}")
+    print(f"Median bad mutation: {median_agb[2]}")
+    print(f"Median all mutation: {median_agb[0]}\n")
+
+    print(f"Deviation good mutation: {deviation_agb[1]}")
+    print(f"Deviation bad mutation: {deviation_agb[2]}")
+    print(f"Deviation all mutation: {deviation_agb[0]}\n")
+
+    print(f"Max of couple coefficient: {fit_values[1][1]}\n"
+          f"    for coil_t={fit_values[1][0]}\n")
+    print(f"Min of couple coefficient: {fit_values[0][1]}\n"
+          f"    for coil_t={fit_values[0][0]}")
+
 
 if __name__ == "__main__":
     main()
